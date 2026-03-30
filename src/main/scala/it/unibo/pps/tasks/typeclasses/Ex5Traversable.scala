a2b7602255c912eeea07005ad89039442d976c26
Francesco Marcatelli <francesco.marcatelli@studio.unibo.it>
2026-03-30 15:52:52 +0200

    Task 5 done

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package it.unibo.pps.tasks.typeclasses
 
-import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.Sequences.Sequence
+import Sequence.*
+import it.unibo.pps.u03.Optionals.Optional
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -23,4 +26,28 @@ object Ex5Traversable:
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
-  
+  trait Traversable[T[_]]:
+    def consumerFunction[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    override def consumerFunction[A](t: Sequence[A])(f: A => Unit): Unit = t match
+      case Cons(h, t) => f(h); consumerFunction(t)(f)
+      case _ => ()
+
+  given Traversable[Optional] with
+    override def consumerFunction[A](t: Optional[A])(f: A => Unit): Unit = t match
+      case Optional.Just(a) => f(a)
+      case _ => ()
+
+  def traversableAll[T[_]: Traversable, A](t: T[A])(f: A => Unit): Unit =
+    summon[Traversable[T]].consumerFunction(t)(f)
+
+@main def tryTraversable(): Unit =
+  import Ex5Traversable.*
+  val s1 = Cons(1, Cons(2, Cons(3, Nil())))
+  val opt1 = Just(10)
+
+  traversableAll(s1)(log)
+  traversableAll(opt1)(log)
+  traversableAll(s1)(println)
+  traversableAll(opt1)(println)
